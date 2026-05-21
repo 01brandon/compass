2f52a6568d24c776418037d4df3ab0d0f7149967
ezekielkibiego <kibiezekiel@gmail.com>
2026-05-21 10:34:01 +0300

    Add drf-spectacular for OpenAPI schema generation and update requirements

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'cloudinary',
+    'drf_spectacular',
     
     # Local apps
     'articles',
@@ -143,9 +144,26 @@ STATIC_URL = 'static/'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',    
+    ],
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    
+    
 }
 
 # Simple JWT configuration: set token lifetimes
